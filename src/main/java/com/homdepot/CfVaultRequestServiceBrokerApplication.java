795a5b136d30cd788d47275f603cf7ab3be97c64
krujos <jkruck@pivotal.io>
2016-02-01 09:42:29 -0600

    Initial commit

---
+++ src/main/java/com/homdepot/CfVaultRequestServiceBrokerApplication.java
@@ -0,0 +1,12 @@
+package com.homdepot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfVaultRequestServiceBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfVaultRequestServiceBrokerApplication.class, args);
+	}
+}
